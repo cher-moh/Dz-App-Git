aa416e9e65b066c21eebb8202cc8a8d71f0eb155
mohamed-lunix <mohamed-lunix@moh-lunix>
2024-12-25 09:30:14 +0100

    laste update

--- worck.with.javafx.V1/src/main/java/compression/CompressionV1.java
+++ worck.with.javafx.V1/src/main/java/compression/CompressionV1.java
@@ -1,4 +1,5 @@
 package compression;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -10,54 +11,49 @@ import java.util.zip.CheckedOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
-public class CompressionV1 {
- //  private static String SOURCE_FILE = "/home/mo_na_ro_ra/Bureau/text_compfression/abc.jpg";
-   private static String TARGET_FILE = "/home/mo_na_ro_ra/Bureau/mo.zip";
-public void CompressedFile(String selectedF) throws IOException
-{
-   /*****************************************************************************/
-      FileOutputStream fout = new FileOutputStream(TARGET_FILE);
-      CheckedOutputStream checksum = new CheckedOutputStream(fout, new Adler32());
-      ZipOutputStream zout = new ZipOutputStream(checksum);
-      FileInputStream fin = new FileInputStream(selectedF);
-      ZipEntry zipEntry = new ZipEntry(selectedF);
-      zout.putNextEntry(zipEntry);
-      int length;
-      byte[] buffer = new byte[1024];
-      while((length = fin.read(buffer)) > 0) {
-         zout.write(buffer, 0, length);
-      }
-      zout.closeEntry();
-      zout.finish();
-      fin.close();
-      zout.close();
-   
-/*****************************************************************************************/
-  
-      ZipInputStream zin = new ZipInputStream(new FileInputStream(TARGET_FILE)); 
-      ZipEntry entry;
-      while((entry = zin.getNextEntry())!=null){
-         System.out.printf("File: %s Modified on %TD %n", 
-         entry.getName(), new Date(entry.getTime()));
-     
-       
-         zin.closeEntry();
-      }
-      zin.close();
-  /****************************************************************************************/ 
-   
-      FileOutputStream fos = null; 
-      try { 
-         fos = new FileOutputStream(entry.getName()); 
-         while(zin.available() != 0){
-            fos.write(zin.read()); 
-         }
-      } catch (IOException ioex) { 
-         fos.close(); 
-      }
- /***********************************************************************************/
-	
-   
 
-}
+public class CompressionV1 {
+	
+	public void CompressedFile(String selectedF) throws IOException {
+		File fichierS=new File(selectedF);
+		/*********************************get the file and create new zip*******************************************************/
+		try {
+		String Dist ="/home/mohamed-lunix/Bureau/";/*******get distenation when the file zip will be created******/
+		FileOutputStream fileout = new FileOutputStream(Dist+"comp.zip");/*************create new file zip with the name comp.zip**********************************/
+		CheckedOutputStream checksum = new CheckedOutputStream(fileout, new Adler32());/********************crypté le stream ******************************/
+		ZipOutputStream zipout = new ZipOutputStream(checksum);/**********************create new ZIP stream qui va contenir les fichiers a compressé******************************/
+		FileInputStream fin = new FileInputStream(fichierS);/****************get the file will be compressed**********************/
+		ZipEntry zipEntry = new ZipEntry(fichierS.getName());/*******get the name of the file************/
+		zipout.putNextEntry(zipEntry);/************get others file if we have others will be compressed*********************/
+		int length;
+		byte[] buffer = new byte[1024];
+		while ((length = fin.read(buffer)) > 0) {/************read file*****************/
+			zipout.write(buffer, 0, length);
+		}
+		zipout.closeEntry();/****************close all streaming opned ***************/
+		zipout.finish();
+		fin.close();
+		zipout.close();
+		}catch (IOException ioex) {
+			
+		}
+		
+
+		/*****************************************************************************************/
+
+		ZipInputStream zipin = new ZipInputStream(new FileInputStream(fichierS.getPath()));
+		ZipEntry entry;
+		try {
+		while ((entry = zipin.getNextEntry()) != null) {
+			System.out.printf("File: %s Modified on %TD %n", entry.getName(), new Date(entry.getTime()));
+			zipin.closeEntry();
+		}
+		zipin.close();
+		}catch (IOException ioex) {
+			
+		}
+		System.out.print("ok 2 eme partie");
+
+
+	}
 }
