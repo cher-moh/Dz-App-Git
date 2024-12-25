267ece8154c1d84ab25337ed0aeb5fc8df7b32f1
mohamed-lunix <mohamed-lunix@moh-lunix>
2024-12-25 15:53:53 +0100

    15:53

--- worck.with.javafx/src/main/java/application/actions_fenetre.java
+++ worck.with.javafx/src/main/java/application/actions_fenetre.java
@@ -1,4 +1,5 @@
 package application;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -14,111 +15,136 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import compression.lzw;
-
+import compression.CompressionV1;
+import compression.decompressionV1;
 
 public class actions_fenetre implements Initializable {
-	FileChooser filechooser = new FileChooser();
-	lzw compres = new lzw();
-
-	public File selectedFile() {
-		Stage win = new Stage();
-		File selectedFile = filechooser.showOpenDialog(win);
-		return selectedFile;
-	}
-	@FXML
-	private TextField recuptext;
+	/**************************************
+	 * les composants de la fenetre.
+	 ********************************************/
 	@FXML
 	private Pane a1;
 	@FXML
 	private StackPane all;
 	@FXML
-	private Button charger;
+	private Button charger1;
 	@FXML
-	private ToggleGroup level;
+	private Button charger11;
 	@FXML
-	private TextArea idtextresultat;
+	private Button charger111;
+	@FXML
+	private Button charger2;
+	@FXML
+	private TextArea idtextresultat1;
+	@FXML
+	private TextArea idtextresultat2;
+	@FXML
+	private TextField recuptext1;
+	@FXML
+	private TextField recuptext11;
+	@FXML
+	private TextField recuptext12;
+	@FXML
+	private TextField recuptext2;
+
+	/*********************************************
+	 * les actions sur la fenetre
+	 *********************************************/
+	FileChooser filechooser = new FileChooser();
+	lzw compres = new lzw();
+	DirectoryChooser directo=new DirectoryChooser();
+	public File selectedFile() {
+		File selected=null;
+		try {
+		Stage win = new Stage();
+		File selectedFile = filechooser.showOpenDialog(win);	
+		selected=	selectedFile;
+		}
+		catch (Exception e) {System.out.print("No File Selected !!");}
+		return selected;
+	}
+	public File selectedDirect() {
+		File selected=null;
+		try {
+		Stage win = new Stage();
+		File selectedDirectory = directo.showDialog(win);		
+		selected=selectedDirectory;
+		}
+		catch (Exception e) {System.out.print("No Directory Selected !!");}
+		return selected;
+	}
 
 	@FXML
-	void choix(ActionEvent event) {
-		recuptext.setText(selectedFile().getAbsolutePath());
+	void compresser(ActionEvent event) {
+		try {
+			CompressionV1 comp = new CompressionV1();
+			comp.CompressedFile(recuptext1.getText(),recuptext11.getText());
+		} catch (Exception e) {
+			e.printStackTrace();
+		
+		}
+
+	
 	}
+
+	@FXML
+	void Ectracter(ActionEvent event) throws IOException {
+		try {
+			decompressionV1 comp = new decompressionV1();
+			comp.DecompressionFile(recuptext2.getText(),recuptext12.getText());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	@FXML
 	void exit(ActionEvent event) {
 		System.exit(0);
 	}
+
 	@FXML
 	void close(ActionEvent event) {
 		System.exit(0);
 	}
 
 	@FXML
-	void compresser(ActionEvent event) {
+	void choix1(ActionEvent event) {
 		try {
-			File monfichier = new File(recuptext.getText());/// le fichier à écrire
-			String str = "";
-			String i;
-			FileReader freader = new FileReader(monfichier);
-			BufferedReader bfreader = new BufferedReader(freader);
-			FileWriter fwriter = new FileWriter("/home/mohamed-lunix/Bureau/Compression.izap");
-			BufferedWriter bfwriter = new BufferedWriter(fwriter);
-			while ((i = bfreader.readLine()) != null) {
-				str += i;
-			}
-			String comp = compres.lzw_compress(str);
-			String AFF = "/" + monfichier.getName();
-			bfwriter.write(comp);
-			bfwriter.newLine();
-			bfwriter.write(AFF);
-			bfreader.close();
-			bfwriter.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			// idtextfieldresultat.setText("ERROR !!!!!!!!");
-		}
-
-		idtextresultat.setText("Compression LZW Ended");
+		recuptext1.setText(selectedFile().getAbsolutePath());
+		}catch (Exception e) {System.out.print("No File Selected !!");}
 	}
 
 	@FXML
-	void Ectracter(ActionEvent event) throws IOException {
-		try {
-			lzw extraction = new lzw();
-			File monfichier = new File(recuptext.getText());/// le fichier à écrire
-			String str = "";
-			String i;
-			FileReader freader = new FileReader(monfichier);
-			BufferedReader bfreader = new BufferedReader(freader);
-			while ((i = bfreader.readLine()) != null) {
-				str += i;
-			}
-			String j;
-			String extra = "";
-			String name = str.substring(str.indexOf("/") + 1, str.length());
-			FileReader freaderstr = new FileReader(monfichier);
-			BufferedReader bfreaderstr = new BufferedReader(freaderstr);
-			FileWriter fwriterstr = new FileWriter("/home/mohamed-lunix/Bureau/" + name);
-			BufferedWriter bfwriterstr = new BufferedWriter(fwriterstr);
-			while ((j = bfreaderstr.readLine()) != null) {
-				extra += j;
-			}
-			extra = extra.substring(0, str.indexOf("/"));
-			//bfwriterstr.write(extra);
-			//System.out.print(extraction.lzw_extract(extra));
-			bfwriterstr.write(extraction.lzw_extract(extra));
-			bfreader.close();
-			bfreaderstr.close();
-			bfwriterstr.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	void choix2(ActionEvent event) {
+	try {
+		recuptext2.setText(selectedFile().getAbsolutePath());
+	}	catch (Exception e) {System.out.print("No File Selected !!");}
+		
+	}
 
-		idtextresultat.setText("Extraction izap Ended");
+	@FXML
+	void ousortie1(ActionEvent event) {
+		try {
+			
+			recuptext11.setText(selectedDirect().getAbsolutePath());
+		}	catch (Exception e) {System.out.print("No Directory Selected !!");}
+		
+	}
+
+	@FXML
+	void ousortie2(ActionEvent event) {
+		try {
+			recuptext12.setText(selectedDirect().getAbsolutePath());
+			
+		}	catch (Exception e) {System.out.print("No Directory Selected !!");}
 	}
 
 	@Override
--- worck.with.javafx/src/main/java/compression/decompressionV1.java
+++ worck.with.javafx/src/main/java/compression/decompressionV1.java
@@ -3,43 +3,36 @@ package compression;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 import javafx.event.ActionEvent;
-public class  decompressionV1{
-	public File DecompressionFile(File selectedfile) throws IOException {
-	try {
-	System.out.print("ok");
-	String name = selectedfile.getName().substring(0,selectedfile.getName().indexOf("."));
-			String str = "";
-			String i;
-			FileReader freader = new FileReader(selectedfile);
-			BufferedReader bfreader = new BufferedReader(freader);		
-			while ((i = bfreader.readLine()) != null) {
-				str += i;
+
+public class decompressionV1 {
+	public void DecompressionFile(String selectedfile,String directory) throws IOException {
+		File fichierS = new File(selectedfile);
+		try {			
+			ZipInputStream zipin = new ZipInputStream(new FileInputStream(fichierS.getAbsolutePath()));
+			ZipEntry ze = new ZipEntry(zipin.getNextEntry());
+			String namezip = ze.getName();
+			String chemainextract=fichierS.getParent();
+			FileOutputStream fos = new FileOutputStream(chemainextract+"/"+namezip);
+			int length;
+			byte[] buffer = new byte[1024];
+			while ((length = zipin.read(buffer)) > 0) {	
+				fos.write(buffer, 0, length);
 			}
-			String j;
-			String extra = "";
-			FileReader freaderstr = new FileReader(selectedfile);
-			BufferedReader bfreaderstr = new BufferedReader(freaderstr);
-			FileWriter fwriterstr = new FileWriter("/home/mohamed-lunix/Bureau/" + name);
-			BufferedWriter bfwriterstr = new BufferedWriter(fwriterstr);
-			while ((j = bfreaderstr.readLine()) != null) {
-				extra += j;
-			}
-			extra = extra.substring(0, str.indexOf("/"));
-			bfwriterstr.write(extra);
-			System.out.print(extra);
-			//bfwriterstr.write(extraction.lzw_extract(extra));
-			bfreader.close();
-			bfreaderstr.close();
-			bfwriterstr.close();
-		} catch (Exception e) {
-			e.printStackTrace();
+			zipin.close();
+			zipin.closeEntry();
+			fos.close();
+		} catch (IOException ioex) {
+
 		}
-		return selectedfile;/*a voir*/
 	}
 }
-
-
